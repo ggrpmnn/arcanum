c7fc7ccf4d8ceadb73060ae632126409e429ac36
Garrett Graupmann <garrett_graupmann@intuit.com>
2016-11-15 09:54:58 -0800

    Added structs for data and a catch-all for non-existant pages (404s).

--- arcanum.go
+++ arcanum.go
@@ -7,10 +7,40 @@ import (
 )
 
 ///////////////////////////////////////////////////////////////////////////////
+// Types
+///////////////////////////////////////////////////////////////////////////////
+type Spell struct {
+	Name       string
+	Tags       []string
+	Type       string
+	Time       string
+	Range      string
+	Components SpellComponents
+}
+
+type SpellComponents struct {
+	Verbal         bool
+	Somatic        bool
+	Material       bool
+	MaterialString []string
+}
+
+///////////////////////////////////////////////////////////////////////////////
 // Handler functions
 ///////////////////////////////////////////////////////////////////////////////
 func rootHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Spells go here")
+	if r.URL.Path != "/" {
+		errorHandler(w, r, http.StatusNotFound)
+		return
+	}
+	fmt.Fprintln(w, "Welcome to Arcanum! Spells go here!")
+}
+
+func errorHandler(w http.ResponseWriter, r *http.Request, s int) {
+	w.WriteHeader(s)
+	if s == http.StatusNotFound {
+		fmt.Fprintf(w, "Sorry - the page '%s' has been lost to the Weave!", r.URL)
+	}
 }
 
 ///////////////////////////////////////////////////////////////////////////////
